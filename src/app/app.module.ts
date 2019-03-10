4bcf4267529a25e4e071df8d8b7afaf71a29d21a
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-03-10 11:56:25 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {AngularFireModule} from 'angularfire2';
 
+
+export const firebaseconfig = {
+  apiKey: "AIzaSyBYhY8-t3YQpxvH-1zVbhcSynAWKB7YDG4",
+  authDomain: "authfull-80cd1.firebaseapp.com",
+  databaseURL: "https://authfull-80cd1.firebaseio.com",
+  projectId: "authfull-80cd1",
+  storageBucket: "authfull-80cd1.appspot.com",
+  messagingSenderId: "354307653152"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseconfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
